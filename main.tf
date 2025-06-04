040cae6fde8d477a522eed011ee8e2079a588f87
jeremiahb97 <jeremy.barton97@gmail.com>
2025-06-04 11:24:30 -0500

    main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,23 @@
-provider google {
-    
+provider "google" {
+  project = "alien-drake-271918"
+  region  = "us-central1"
+}
+
+resource "google_storage_bucket" "example_bucket" {
+  name          = "mynewbucket9090"
+  location      = "US"
+  storage_class = "STANDARD"
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle_rule {
+    action {
+      type = "Delete"
+    }
+    condition {
+      age = 30
+    }
+  }
 }
