fabfeb95ddc040556b7c798142483fd93db2de91
jeremiahb97 <jeremy.barton97@gmail.com>
2025-06-05 09:35:49 -0500

    main.tf

--- main.tf
+++ main.tf
@@ -21,3 +21,27 @@ resource "google_storage_bucket" "example_bucket" {
     }
   }
 }
+
+provider "google" {
+    project = "alien-drake-271918"
+    region = "us-central1"
+}
+
+resource "google_compute_instance" "example_instance" {
+
+    name = "my-new-instance"
+    machine_type = "e2-medium"
+    zone = "us-central1-a"
+    boot_disk {
+        initialize_params {
+            image = "projects/debian-cloud/global/images/family/debian-10"
+        }
+    }
+    network_interface {
+        network = "default"
+        access_config {
+            name = "External NAT"
+            network_tier = "PREMIUM"
+        }
+    }
+}
